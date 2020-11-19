21ce8cd6c8ad4b60f01f560405f5bd52a1506793
ShushingWong <122181582@qq.com>
2020-11-20 00:04:14 +0800

    test

--- test/Test.cs
+++ test/Test.cs
@@ -7,11 +7,11 @@ namespace Pomelo.DotNetClient.Test
     {
         public static void Main(string[] args)
         {
-            byte[] bytes = Pomelo.Protobuf.Encoder.encodeUInt32(112321);
-            Console.WriteLine(Pomelo.Protobuf.Decoder.decodeUInt32(bytes));
-            CodecTest.Run();
-            ProtobufTest.Run();
-            TransportTest.Run();
+            //byte[] bytes = Pomelo.Protobuf.Encoder.encodeUInt32(112321);
+            //Console.WriteLine(Pomelo.Protobuf.Decoder.decodeUInt32(bytes));
+            //CodecTest.Run();
+            //ProtobufTest.Run();
+            //TransportTest.Run();
             ClientTest.Run();
             Console.ReadKey();
         }
